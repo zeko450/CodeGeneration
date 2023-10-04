5429d846bdde9afb8096fff07264fc8bf623763d
Mohamed Kachach <114595632+zeko450@users.noreply.github.com>
2023-10-03 22:52:19 -0400

    Add files via upload

---
+++ CodeGeneration-master/ddl Access.sql
@@ -0,0 +1,14 @@
+drop table access;
+create database codegeneration;
+use codegeneration;
+create table Access (
+id int(4) Primary Key AUTO_INCREMENT,
+platform varchar(50) not null,
+password varchar(12) not null,
+userkey varchar(40) not null
+);
+
+##use codegeneration;
+insert into Access(platform, password, userkey) values ('Deloitte','Gvwchwlrwc1&',' f110U44wq322`[V0}433lga10VP4xrm22a\\W0~y');
+select * from access;
+
