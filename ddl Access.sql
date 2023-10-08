1f6213aa8deadc02dfc742458b8a5e5b250d576a
Mohamed Kachach <114595632+zeko450@users.noreply.github.com>
2023-10-08 01:02:29 -0400

    ddl Access.sql

--- ddl Access.sql
+++ ddl Access.sql
@@ -1,4 +1,3 @@
-drop table access;
 create database codegeneration;
 use codegeneration;
 create table Access (
@@ -8,7 +7,4 @@ password varchar(12) not null,
 userkey varchar(40) not null
 );
 
-##use codegeneration;
-insert into Access(platform, password, userkey) values ('Deloitte','Gvwchwlrwc1&',' f110U44wq322`[V0}433lga10VP4xrm22a\\W0~y');
-select * from access;
 
